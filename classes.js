9a4151552ca799888ff115dfd964d459377e4ca5
Jenesh <jenesh.napit@gmail.com>
2019-07-29 23:28:46 -0400

    some progress

--- classes.js
+++ classes.js
@@ -8,7 +8,9 @@ class Person {
         return `${this.first} ${this.middle} ${this.last}`;
     }
 }
-
+let aPerson = new Person('Just', 'Do', 'It');
+console.log(aPerson);
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - -');
 class Book {
     constructor(title, author, rating) {
         this.title = title;
@@ -18,12 +20,15 @@ class Book {
 }
 
 let book1 = new Book('You Don\'t know JS', 'Unknown', 10);
-let book1 = new Book('Principles, 'Ray Dalio', 8);
+let book2 = new Book('Principles', 'Ray Dalio', 8);
 
 Book.prototype.isGood = function() {
     return this.rating > 7;
 }
-
+console.log(book1);
+console.log(book2);
+console.log("Good? ", book1.isGood());
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - -');
 class Dog {
     constructor(name, breed, mood, hungry) {
         this.name = name;
@@ -34,6 +39,29 @@ class Dog {
     playFetch() {
         this.hungry = true;
         this.mood = 'playful';
-        console.log('Ruff!!');
+        console.log('Ruff!');
     }
 }
+Dog.prototype.feed = function() {
+    if (this.hungry) {
+        this.hungry = false;
+        console.log('Woof!');
+    } else {
+        console.log('You tryna make me fat?!');
+    }
+}
+Dog.prototype.toString = function() {
+    return `${this.name} is a beautiful ${this.breed} dog. Right now he is ${this.mood} and ${this.hungry ? 'hungry' : 'not hungry'}.`;
+}
+let doggo = new Dog('Lika', 'German Sheppard', 'happy', true);
+console.log(doggo);
+console.log(doggo.playFetch());
+console.log(doggo.feed());
+console.log(doggo.toString());
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - -');
+const feezingPoint = {
+    celsius: 0,
+    fahrenheit: 32,
+    kelvin: 273.2
+};
+
